b3d83b45e2e6092769d6a168bd45a22c964ac415
Ivan <example@gmail.com>
2019-04-27 22:33:09 +0300

    Это наш первый commit

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+ let person={
+     name: "John",
+     age: 25,
+     isMarried: false
+ };
+
+ console.log(person["name"]);
+
+ let arr = ['plum.jpg','orange.jpg'];
+ console.log(arr[1]);
+
+ //alert("hello");
+// let answer = confirm("Are you here");
+ //console.log(answer);
+
+ let answer = prompt("Are you here");
+ console.log(answer);
